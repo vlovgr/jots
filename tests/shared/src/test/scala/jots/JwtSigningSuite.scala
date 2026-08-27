ba7ffc9134980efe062c76d802d1c3fe601df004
Viktor Rudebeck <viktor@rudebeck.nu>
2026-08-27 08:45:47 +0200

    Add `JwtSigning.default#jwk` tests

--- tests/shared/src/test/scala/jots/JwtSigningSuite.scala
+++ tests/shared/src/test/scala/jots/JwtSigningSuite.scala
@@ -19,14 +19,26 @@ package jots
 import cats.data.NonEmptyList
 import cats.effect.IO
 import cats.syntax.all.*
+import io.circe.Json
+import io.circe.syntax.*
+import jots.JwtException.InvalidAlgorithm
 import jots.JwtException.InvalidEcKeyLength
+import jots.JwtException.InvalidKeyId
 import jots.JwtException.InvalidPrivateKey
 import jots.JwtException.InvalidRsaKeyLength
 import jots.JwtException.InvalidSecretKeyLength
+import jots.JwtException.MissingKeyId
+import jots.JwtException.RejectedAlgorithm
+import jots.JwtException.UnsuitableSigningKey
+import jots.JwtException.UnsupportedKey
+import jots.JwtHmacAlgorithm.HS256
 import jots.crypto.PrivateKey
+import jots.crypto.SecretKey
 import jots.crypto.internal.asn1.Asn1
 import jots.crypto.internal.asn1.Oid
 import jots.testing.*
+import jots.testing.syntax.*
+import org.scalacheck.Gen
 import scodec.bits.ByteVector
 import weaver.SimpleIOSuite
 import weaver.scalacheck.Checkers
@@ -140,4 +152,238 @@ object JwtSigningSuite extends SimpleIOSuite with Checkers {
       }
     }
   }
+
+  test("JwtSigning.jwk") {
+    val gen =
+      for {
+        builder <- jwtBuilderGen
+        (algorithm, privateKey, publicKey) <- signingKeyGen
+      } yield (algorithm, builder, privateKey, publicKey)
+
+    forall(gen) { case (algorithm, builder, privateKey, publicKey) =>
+      for {
+        signing <- JwtSigning.default[IO].jwk(algorithm, privateKey)
+        verification <- JwtVerificationBuilder
+          .default[IO]
+          .jwkSet(NonEmptyList.of(algorithm), JwkSet(publicKey))
+          .withCheckExpiration(false)
+          .withCheckIssuedAt(false)
+          .withCheckNotBefore(false)
+          .build
+        signed <- builder.signWith(signing)
+        _ <- signed.verifyWith(verification)
+      } yield success
+    }
+  }
+
+  test("JwtSigning.jwk.setsKeyId") {
+    for {
+      signing <- JwtSigning.default[IO].jwk(HS256, octJwk(secretKey))
+      signed <- JwtBuilder(JwtHeader.default, JwtClaims.empty).signWith(signing)
+    } yield expect.eql(Some(keyId.value), signed.header.toJsonObject("kid").flatMap(_.asString))
+  }
+
+  test("JwtSigning.jwk.rejectMissingKeyId") {
+    val key =
+      jwk(
+        "kty" -> "oct".asJson,
+        "k" -> secretKey.toByteVector.toBase64UrlNoPad.asJson
+      )
+
+    JwtSigning.default[IO].jwk(HS256, key).attempt.map {
+      case Left(_: MissingKeyId) => success
+      case _ => failure("unexpected case")
+    }
+  }
+
+  test("JwtSigning.jwk.rejectInvalidKeyId") {
+    val key = octJwk(secretKey, "kid" -> 1.asJson)
+
+    JwtSigning.default[IO].jwk(HS256, key).attempt.map {
+      case Left(_: InvalidKeyId) => success
+      case _ => failure("unexpected case")
+    }
+  }
+
+  test("JwtSigning.jwk.rejectKeyForEncryptionUse") {
+    val key = octJwk(secretKey, "use" -> "enc".asJson)
+
+    JwtSigning.default[IO].jwk(HS256, key).attempt.map {
+      case Left(_: UnsuitableSigningKey) => success
+      case _ => failure("unexpected case")
+    }
+  }
+
+  test("JwtSigning.jwk.acceptsKeyForSignatureUse") {
+    val key = octJwk(secretKey, "use" -> "sig".asJson)
+
+    JwtSigning.default[IO].jwk(HS256, key).attempt.map {
+      case Right(_) => success
+      case _ => failure("unexpected case")
+    }
+  }
+
+  test("JwtSigning.jwk.rejectKeyWithoutSignKeyOperation") {
+    val key = octJwk(secretKey, "key_ops" -> List("verify").asJson)
+
+    JwtSigning.default[IO].jwk(HS256, key).attempt.map {
+      case Left(_: UnsuitableSigningKey) => success
+      case _ => failure("unexpected case")
+    }
+  }
+
+  test("JwtSigning.jwk.acceptsKeyWithSignKeyOperation") {
+    val key = octJwk(secretKey, "key_ops" -> List("sign", "verify").asJson)
+
+    JwtSigning.default[IO].jwk(HS256, key).attempt.map {
+      case Right(_) => success
+      case _ => failure("unexpected case")
+    }
+  }
+
+  test("JwtSigning.jwk.rejectInvalidKeyOperations") {
+    val key = octJwk(secretKey, "key_ops" -> "sign".asJson)
+
+    JwtSigning.default[IO].jwk(HS256, key).attempt.map {
+      case Left(_: UnsuitableSigningKey) => success
+      case _ => failure("unexpected case")
+    }
+  }
+
+  test("JwtSigning.jwk.acceptsMatchingAlgorithm") {
+    val key = octJwk(secretKey, "alg" -> HS256.name.asJson)
+
+    JwtSigning.default[IO].jwk(HS256, key).attempt.map {
+      case Right(_) => success
+      case _ => failure("unexpected case")
+    }
+  }
+
+  test("JwtSigning.jwk.rejectMismatchedAlgorithm") {
+    val key = octJwk(secretKey, "alg" -> JwtHmacAlgorithm.HS384.name.asJson)
+
+    JwtSigning.default[IO].jwk(HS256, key).attempt.map {
+      case Left(_: RejectedAlgorithm) => success
+      case _ => failure("unexpected case")
+    }
+  }
+
+  test("JwtSigning.jwk.rejectInvalidAlgorithm") {
+    val key = octJwk(secretKey, "alg" -> 256.asJson)
+
+    JwtSigning.default[IO].jwk(HS256, key).attempt.map {
+      case Left(_: InvalidAlgorithm) => success
+      case _ => failure("unexpected case")
+    }
+  }
+
+  test("JwtSigning.jwk.rejectMismatchedKeyType") {
+    forall(jwkEcdsaPrivateKeyGen) { key =>
+      JwtSigning.default[IO].jwk(HS256, withKeyId(key)).attempt.map {
+        case Left(_: UnsupportedKey) => success
+        case _ => failure("unexpected case")
+      }
+    }
+  }
+
+  test("JwtSigning.jwk.rejectUnsupportedKeyType") {
+    val key = octJwk(secretKey, "kty" -> "unknown".asJson)
+
+    JwtSigning.default[IO].jwk(HS256, key).attempt.map {
+      case Left(_: UnsupportedKey) => success
+      case _ => failure("unexpected case")
+    }
+  }
+
+  test("JwtSigning.jwk.rejectPublicKey") {
+    val gen = Gen.oneOf(ecdsaSigningKeyGen, eddsaSigningKeyGen, rsaSigningKeyGen)
+
+    forall(gen) { case (algorithm, _, publicKey) =>
+      JwtSigning.default[IO].jwk(algorithm, publicKey).attempt.map {
+        case Left(_: InvalidPrivateKey) => success
+        case _ => failure("unexpected case")
+      }
+    }
+  }
+
+  private val keyId: JwkKeyId =
+    JwkKeyId("key-1")
+
+  private val secretKey: SecretKey =
+    secretKey"a-string-secret-at-least-256-bits-long"
+
+  /**
+    * Generates an algorithm along with a private and public key for
+    * the algorithm, where both keys have [[keyId]] as their key id.
+    */
+  private val signingKeyGen: Gen[(JwtAlgorithm, Jwk, Jwk)] =
+    Gen.oneOf(ecdsaSigningKeyGen, eddsaSigningKeyGen, hmacSigningKeyGen, rsaSigningKeyGen)
+
+  private lazy val ecdsaSigningKeyGen: Gen[(JwtAlgorithm, Jwk, Jwk)] =
+    jwkEcdsaKeyPairGen.flatMap { case (privateKey, publicKey) =>
+      val algorithmGen =
+        privateKey.toJsonObject("crv").flatMap(_.asString) match {
+          case Some("P-256") => Gen.const(JwtEcdsaAlgorithm.ES256)
+          case Some("P-384") => Gen.const(JwtEcdsaAlgorithm.ES384)
+          case Some("P-521") => Gen.const(JwtEcdsaAlgorithm.ES512)
+          case _ => Gen.fail
+        }
+
+      algorithmGen.map { algorithm =>
+        (algorithm, withKeyId(privateKey), withKeyId(publicKey))
+      }
+    }
+
+  private lazy val eddsaSigningKeyGen: Gen[(JwtAlgorithm, Jwk, Jwk)] =
+    jwkEddsaKeyPairGen.flatMap { case (privateKey, publicKey) =>
+      val algorithmGen =
+        privateKey.toJsonObject("crv").flatMap(_.asString) match {
+          case Some("Ed448") => Gen.const(JwtEddsaAlgorithm.Ed448)
+          case Some("Ed25519") => Gen.const(JwtEddsaAlgorithm.Ed25519)
+          case _ => Gen.fail
+        }
+
+      algorithmGen.map { algorithm =>
+        (algorithm, withKeyId(privateKey), withKeyId(publicKey))
+      }
+    }
+
+  private lazy val hmacSigningKeyGen: Gen[(JwtAlgorithm, Jwk, Jwk)] =
+    for {
+      algorithm <- jwtHmacAlgorithmGen
+      secretKey <- secretKeyMinLengthGen(algorithm.minKeyLength)
+      key = octJwk(secretKey)
+    } yield (algorithm, key, key)
+
+  private lazy val rsaSigningKeyGen: Gen[(JwtAlgorithm, Jwk, Jwk)] =
+    jwkRsaKeyPairGen.flatMap { case (privateKey, publicKey) =>
+      jwtRsaAlgorithmGen.map { algorithm =>
+        (algorithm, withKeyId(privateKey), withKeyId(publicKey))
+      }
+    }
+
+  private def jwk(fields: (String, Json)*): Jwk =
+    Jwk(fields: _*).fold(throw _, identity)
+
+  private def octJwk(secretKey: SecretKey, fields: (String, Json)*): Jwk =
+    withFields(
+      jwk(
+        "kty" -> "oct".asJson,
+        "kid" -> keyId.asJson,
+        "k" -> secretKey.toByteVector.toBase64UrlNoPad.asJson
+      ),
+      fields: _*
+    )
+
+  private def withKeyId(key: Jwk): Jwk =
+    withFields(key, "kid" -> keyId.asJson)
+
+  private def withFields(key: Jwk, fields: (String, Json)*): Jwk =
+    Jwk
+      .fromJsonObject {
+        fields.foldLeft(key.toJsonObject) { case (jsonObject, (name, value)) =>
+          jsonObject.add(name, value)
+        }
+      }
+      .fold(throw _, identity)
 }
